d96c69d1e43b1df2daaa7c5440bb9d23efa5d262
Dia <diaduckk@outlook.com>
2025-07-22 00:13:15 +0100

    rstrip did nothing ? also 9.txt i guess

--- generator/generate_save.py
+++ generator/generate_save.py
@@ -20,7 +20,7 @@ def generate_and_save(prompt, model_dir, output_file, max_length=20, num_return_
     with open(output_file, "w", encoding="utf-8") as f:
         for i, output in enumerate(results, 1):
             f.write(f"\n\n\n=== Output {i} ===\n")
-            f.write(output['generated_text'] + "")
+            f.write(output['generated_text'.rstrip()])
 
     print(f"Saved {len(results)} generated sequences to '{output_file}'")
 
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         prompt=changeablePrompt,
         model_dir="generator/model/gpt2-jinvithoughts",
         #output_file=f"Generated Outputs/Prompts/{changeablePrompt}",
-        output_file="Generated Outputs/Prompt-Free/8.txt",
+        output_file="Generated Outputs/Prompt-Free/9.txt",
         max_length=20,
         num_return_sequences=20
     )
